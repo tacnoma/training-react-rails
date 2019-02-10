37eda1984667ba60499d44e9ef0f2e164626c5af
tacnoma <ctchnm.geo@gmail.com>
2019-02-10 17:33:52 +0900

    Add: rails g controller and channel for chat

---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,15 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    stream_from "chat_channel"
+    ActionCable.server.broadcast 'chat_channel', message: 'connected.'#
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    ActionCable.server.broadcast 'chat_channel', message: data['message']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'chat/show'
+
   root 'comments#index'
   namespace :api, format: 'json' do
     namespace :v1 do

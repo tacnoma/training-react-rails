73ff083b9be52eba3b6c269bf475f2e2b6689677
tacnoma <ctchnm.geo@gmail.com>
2019-02-11 01:03:50 +0900

    Fix: trial impl for input ongoing notify

--- app/assets/javascripts/components/chat.js.jsx
+++ app/assets/javascripts/components/chat.js.jsx
@@ -1,10 +1,10 @@
 class Chat extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {comments: []};
-
+    this.state = {comments: [], ongoing: []};
     this.updateMessage = this.updateMessage.bind(this);
     this.setupSubscription = this.setupSubscription.bind(this);
+    this.speak = this.speak.bind(this);
   }
 
   componentDidMount() {
@@ -12,28 +12,55 @@ class Chat extends React.Component {
   }
 
   updateMessage(comment) {
-    this.setState({
-      comments: this.state.comments.concat({
-        author: comment.author, text: comment.text
+    if (comment.text) {
+      this.setState({
+        comments: this.state.comments.concat({
+          id: comment.id, author: comment.author, text: comment.text
+        }),
+        ongoing: this.state.ongoing
       })
-    })
+    } else if (comment.ongoing ) {
+      if(this.state.ongoing.indexOf(comment.author) === - 1) {
+        this.setState({
+          comments: this.state.comments,
+          ongoing: this.state.ongoing.concat([comment.author])
+        })
+      }
+    } else if (comment.done) {
+      let index = this.state.ongoing.indexOf(comment.author);
+      const newOngoing = this.state.ongoing;
+      if (index !== -1) {
+        newOngoing.splice(index, 1);
+      }
+      this.setState({
+        comments: this.state.comments,
+        ongoing: newOngoing
+      })
+    }
   }
 
   setupSubscription() {
-    App.cable.subscriptions.create('ChatChannel', {
+    this.appCable = App.cable.subscriptions.create('ChatChannel', {
       received: (comment) => {
         this.updateMessage(comment);
       },
     });
   }
 
+  speak(comment) {
+    this.appCable.perform('speak', comment);
+  }
+
   render() {
     return (
       <div className='comments'>
-        {this.state.comments.map(function (comment, i) {
-          return <p key={i}><b>{comment.author}:</b>{comment.text}</p>
+        {this.state.comments.map((comment) => {
+          return <p key={comment.id}><b>{comment.author}:</b>{comment.text}</p>
         })}
-        <ChatCommentForm url={this.props.url} />
+        <span className='ongoing-authors'>
+          <p>{this.state.ongoing.join(',') + (this.state.ongoing.length > 0 ? ' typing...' : '')}</p>
+        </span>
+        <ChatCommentForm url={this.props.url} handleNotifyOngoing={this.speak} />
       </div>
     )
   }
@@ -42,12 +69,14 @@ class Chat extends React.Component {
 class ChatCommentForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { inputOngoing: false };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.onCommentSubmit = this.onCommentSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   onCommentSubmit(comment)  {
-    fetch(this.props.url + '?from_chat=1', {
+    fetch(this.props.url, {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, cors, *same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -68,6 +97,23 @@ class ChatCommentForm extends React.Component {
       );
   }
 
+  handleChange(e) {
+    e.preventDefault();
+    let author = this.author.value.trim();
+    let text = this.text.value.trim();
+    if(!text)  {
+      let comment = {author: author, done: true};
+      this.props.handleNotifyOngoing(comment);
+      this.state.inputOngoing = false;
+      return;
+    }
+    if (!this.state.inputOngoing) {
+      this.inputOngoing = true;
+      let comment = {author: author, ongoing: true};
+      this.props.handleNotifyOngoing(comment);
+    }
+  }
+
   handleSubmit(e) {
     e.preventDefault();
     let author = this.author.value.trim();
@@ -76,13 +122,14 @@ class ChatCommentForm extends React.Component {
       return;
     }
     this.onCommentSubmit({author: author, text: text});
-    this.author.value = '';
+    this.inputOngoing = false;
+    this.props.handleNotifyOngoing({author: author, done: true});
     this.text.value = '';
   }
 
   render() {
     return (
-      <form className="commentForm" onSubmit={this.handleSubmit}>
+      <form className="commentForm" onSubmit={this.handleSubmit} onChange={this.handleChange}>
         <input type="text" placeholder="Your name" ref={(author) => { this.author = author; }}/>
         <input type="text" placeholder="Say something..." ref={(text) => { this.text = text; }} />
         <input type="submit" value="Post" />
--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -9,6 +9,12 @@ class ChatChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    ActionCable.server.broadcast 'chat_channel', text: data['text'], author: data['author']
+    if data['text']
+      ActionCable.server.broadcast 'chat_channel', text: data['text'], author: data['author']
+    elsif data['ongoing']
+      ActionCable.server.broadcast 'chat_channel', ongoing: true, author: data['author']
+    elsif data['done']
+      ActionCable.server.broadcast 'chat_channel', done: true, author: data['author']
+    end
   end
 end

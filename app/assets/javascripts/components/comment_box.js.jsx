6fbb971f95729f3268d3a2e74e18f17fabf1161a
tacnoma <ctchnm.geo@gmail.com>
2019-02-10 15:44:03 +0900

    Fix: React.createClass to Reacto.Component

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -1,5 +1,10 @@
-var CommentBox = React.createClass({
-  handleCommentSubmit: function(comment) {
+class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { data: [{ Text: 'hola' }] };
+    }
+
+    handleCommentSubmit(comment)  {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -12,11 +17,9 @@ var CommentBox = React.createClass({
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
-  },
-  getInitialState: function() {
-    return {data: []};
-  },
-  loadCommentsFromServer: function() {
+  }
+
+  loadCommentsFromServer() {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -27,24 +30,29 @@ var CommentBox = React.createClass({
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
-  },
-  componentDidMount: function() {
+  }
+
+  componentDidMount() {
     this.loadCommentsFromServer();
     setInterval(this.loadCommentsFromServer, this.props.pollInterval);
-  },
-  render: function() {
+  }
+
+  render() {
     return (
-      <div className="commentBox">
+      <div className="CommentBox">
       <h1>Comments</h1>
       <CommentList data={this.state.data}/>
       <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
-});
+}
 
-var CommentList = React.createClass({
-  render: function() {
+class CommentList extends React.Component {
+  constructor(props) {
+      super(props);
+  }
+  render() {
     var commentNodes = this.props.data.map(function (comment) {
       return (
         <Comment author={comment.author}>{comment.text}</Comment>
@@ -56,11 +64,16 @@ var CommentList = React.createClass({
       </div>
     );
   }
-});
+}
 
-var Comment = React.createClass({
-  render: function() {
-    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+class Comment extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+  render() {
+
+    //var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    var rawMarkup = 'test';
     return (
       <div className="comment">
       <h2 className="commentAuthor">
@@ -70,10 +83,13 @@ var Comment = React.createClass({
       </div>
     );
   }
-});
+}
 
-var CommentForm = React.createClass({
-  handleSubmit: function(e) {
+class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+  handleSubmit(e) {
     e.preventDefault();
     var author = this.author.value.trim();
     var text = this.text.value.trim();
@@ -83,8 +99,9 @@ var CommentForm = React.createClass({
     this.props.onCommentSubmit({author: author, text: text});
     this.author.value = '';
     this.text.value = '';
-  },
-  render: function() {
+  }
+
+  render() {
     return (
       <form className="commentForm" onSubmit={this.handleSubmit}>
         <input type="text" placeholder="Your name" ref={(author) => { this.author = author; }}/>
@@ -93,4 +110,4 @@ var CommentForm = React.createClass({
       </form>
     );
   }
-});
+}
